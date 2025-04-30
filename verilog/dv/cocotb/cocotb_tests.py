e98896805e1c4dd709d7f91ce3d33b86d6e47e3a
bahatoprak <73892150+bahatoprak@users.noreply.github.com>
2025-04-30 14:53:41 +0300

    Initialize stress_tests

---
+++ verilog/dv/cocotb/cocotb_tests.py
@@ -0,0 +1,14 @@
+from hello_world.hello_world import hello_world
+from hello_world_uart.hello_world_uart import hello_world_uart
+from user_proj_tests.counter_wb.counter_wb import counter_wb
+from user_proj_tests.counter_la.counter_la import counter_la
+from user_proj_tests.counter_la_reset.counter_la_reset import counter_la_reset
+from user_proj_tests.counter_la_clk.counter_la_clk import counter_la_clk
+from gpio_test.gpio_test import gpio_test
+
+from user_proj_tests.stress_tests.stress_test import test_aes_sbox
+from user_proj_tests.stress_tests.stress_test import test_hash
+from user_proj_tests.stress_tests.stress_test import test_chacha
+from user_proj_tests.stress_tests.stress_test import test_stress
+from user_proj_tests.stress_tests.stress_test import test_xtea
+from user_proj_tests.stress_tests.stress_test import soft_float
